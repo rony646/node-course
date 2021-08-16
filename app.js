195fb6f1eb52450e704ca16ecf519f282eb68e33
Rony Peterson Assuncao Monteiro Silva <rony.silva@mundiale.com.br>
2021-08-16 15:11:49 -0300

    chore: writing message into a file

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url  = req.url;
+    const method = req.method;
+
+
+    if(url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) =>  {
+            console.log('Chunk: ', chunk)
+            body.push(chunk)
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First App</title></head>');
