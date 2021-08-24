09209d0e73f08f50738bca59ff9672be941339c2
Rony Peterson Assuncao Monteiro Silva <rony.silva@mundiale.com.br>
2021-08-24 20:35:19 -0300

    installed express.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
+
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+
+const server = http.createServer(app);
 
 server.listen(3000);
