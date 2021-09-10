0dcaa41c70f6d2ddf2fdc8c1e247a54d7a8c64e2
Rony Peterson Assuncao Monteiro Silva <rony.silva@mundiale.com.br>
2021-09-10 20:13:27 -0300

    ADDING HTML FILES

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, nex) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
-
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
-
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
