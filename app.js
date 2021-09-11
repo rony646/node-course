e0f9f0e1380b52653fe1ce36be903a0daec40b3c
Rony Peterson Assuncao Monteiro Silva <rony.silva@mundiale.com.br>
2021-09-11 16:28:53 -0300

    starting with PUG

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const path = require('path');
 
 const express = require('express');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, nex) => {
